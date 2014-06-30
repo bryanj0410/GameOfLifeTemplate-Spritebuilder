c0969799562e645d7844b7874911426e3938d8ec
Bryan Johnson <bjohnson@Bryans-Mac-mini.local>
2014-06-30 11:39:24 -0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -169,7 +169,7 @@ static const int GRID_COLUMNS = 10;
                 currentCreature.isAlive = TRUE;
                 numAlive++;
             }
-            else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4)
+            else if (currentCreature.livingNeighbors <= 2 || currentCreature.livingNeighbors >= 4)
             {
                 currentCreature.isAlive = FALSE;
             }
